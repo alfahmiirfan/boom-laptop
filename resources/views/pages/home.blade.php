054f05cba31bcacbad388bef4a6872ab5368d239
alfahmiirfan <alfahmiirfan11@gmail.com>
2025-02-12 22:35:53 +0700

    aseets

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,8 +6,24 @@
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 </head>
 <body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <x-navbar></x-navbar>
+    <div class="max-w-screen-xl mx-auto pr-8 pl-8">
+        {{-- <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1> --}}
+        <div>
+            <h1 class="font-semibold">Best Quality Parts</h1>
+        </div>
+        <div>
+            <div class="grid grid-cols-2 gap-4">
+                <div>
+                    <h1 class="font-semibold">Memperbaiki Hari Anda dengan Teknologi Terbaik</h1>
+                </div>
+                <div>
+                    <img src="assets/home-1.png" class="" alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
